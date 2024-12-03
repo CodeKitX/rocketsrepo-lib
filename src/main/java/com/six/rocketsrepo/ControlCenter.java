d1688c5b904984737719c370e1487cafb0ed7eaa
Łukasz <codekitx@gmail.com>
2024-12-03 22:04:38 +0100

    SIX initial commit - scaffolding of methods and tests

---
+++ src/main/java/com/six/rocketsrepo/ControlCenter.java
@@ -0,0 +1,44 @@
+package com.six.rocketsrepo;
+
+import java.util.List;
+import java.util.Set;
+
+public class ControlCenter {
+
+    void addRocket(Rocket rocket) {
+
+    }
+
+    void scheduleMission(Mission mission) {
+
+    }
+
+    void assignRocket(Rocket rocket, Mission mission) {
+
+    }
+
+    void assignRockets(Set<Rocket> rockets, Mission mission) {
+
+    }
+
+    void startMission(Mission mission) {
+
+    }
+
+    void endMission(Mission mission) {
+
+    }
+
+    void sendRocketToRepair(Rocket rocket) {
+
+    }
+
+    void releaseRocketFromRepari(Rocket rocket) {
+
+    }
+
+    List<Mission> missionStatusReport() {
+        return null;
+    }
+
+}
---
+++ src/test/groovy/com/six/rocketsrepo/ControlCenterSpec.groovy
@@ -0,0 +1,76 @@
+package com.six.rocketsrepo
+
+import spock.lang.Specification
+import spock.lang.Subject
+
+class ControlCenterSpec extends Specification {
+
+    @Subject
+    ControlCenter controlCenter;
+
+    def "should add a rocket to repo"() {
+
+    }
+
+    def "should not duplicate rockets in repo"() {
+
+    }
+
+    def "should assign a rocket to a mission"() {
+
+    }
+
+    def "should not assign an all ready assigned rocket to a mission"() {
+
+    }
+
+    def "should change rocket status"() {
+
+    }
+
+    def "should not reset status of assigned rocket"() {
+
+    }
+
+    def "should add a new mission"() {
+
+    }
+
+    def "should not duplicate missions"() {
+
+    }
+
+    def "should assign multiple rockets to mission"() {
+
+    }
+
+    def "should not assign an all ready assigned rockets to a mission"() {
+
+    }
+
+    def "should change mission status"() {
+
+    }
+
+    def "should not reset mission status"() {
+
+    }
+
+    def "should change mission status when assigned rockets status changes"() {
+
+    }
+
+    def "should not start a mission if rockets are not assigned"() {
+
+    }
+
+    def "should not start a mission when rockets are in repair"() {
+
+    }
+
+    def "should not end mission with assigned rockets" () {
+
+    }
+
+
+}
